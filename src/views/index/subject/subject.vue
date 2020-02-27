b04288b5d5d94a16c2256f1a9efe1db0aeb13a95
qqpp <4399.com>
2020-02-27 17:17:06 +0800

    买菜前储存

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -29,30 +29,99 @@
       </el-form>
     </el-card>
     <el-card class="box-card">
-      <div v-for="o in 4" :key="o" class="text item">
-        {{ "列表内容 " + o }}
-      </div>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column type="index" width="50" label="序号">
+        </el-table-column>
+        <el-table-column prop="rid" label="学科编号"> </el-table-column>
+        <el-table-column prop="name" label="学科名称"> </el-table-column>
+        <el-table-column prop="short_name" label="简称"> </el-table-column>
+        <el-table-column prop="username" label="创建者"> </el-table-column>
+        <el-table-column prop="create_time" label="创建日期"> </el-table-column>
+        <el-table-column prop="status" label="状态">
+          <!-- 自定义列   -->
+          <template slot-scope="scope">
+            <span v-if="scope.row.status == 1">启用</span>
+            <span v-else style="color:red">禁用</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column fixed="right" label="操作">
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)" type="text" size="small"
+              >编辑</el-button
+            >
+            <el-button type="text" size="small">禁用</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="page"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="size"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
+import { get_subject } from "@/api/subject.js";
 export default {
   props: {},
   //数据
   data() {
     return {
-      formInline: ""
+      formInline: {},
+      page: 1, //当前页
+      size: 10, //页容量
+      total: 0, //数据总数量
+      tableData: [] //表格数据
     };
   },
   //方法
-  methods: {},
+  methods: {
+    //表格
+    handleClick(row) {
+      console.log(row);
+    },
+
+    //分页插件
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  },
   //计算属性
   computed: {},
   //过滤器
   filters: {},
   //进入页面就执行的生命周期,可以访问dom
-  created() {},
+  created() {
+    get_subject().then(res => {
+      console.log("表格数据:", res);
+
+      this.tableData = res.data.data.items;
+      this.total = res.data.data.pagination.total;
+      this.page = res.data.data.pagination.page;
+      // this.tableData.create_time = this.tableData.create_time.split("")[0];
+      // this.tableData.create_time = this.tableData.create_time.map(item => {
+      //   if (item) {
+      //     this.tableData.create_time = item.split("]")[0];
+      //   } else {
+      //     return 0;
+      //   }
+      // });
+    });
+  },
   //渲染页面后执行的生命周期,不能访问dom
   mounted() {},
   //侦听器
