4a519c86c105016c42dbe01e7a3e6a1096d93680
qqpp <4399.com>
2020-02-27 13:54:07 +0800

    学科列表第一层布局

--- src/views/index/subject/subject.vue
+++ src/views/index/subject/subject.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>我是学科列表</div>
+  <div>
+    <el-card class="box-card">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="学科编号">
+          <el-input class="duan" v-model="formInline.user"></el-input>
+        </el-form-item>
+        <el-form-item label="学科名称">
+          <el-input class="chang" v-model="formInline.user"></el-input>
+        </el-form-item>
+        <el-form-item label="创建者">
+          <el-input class="duan" v-model="formInline.user"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-select
+            class="chang"
+            v-model="formInline.region"
+            placeholder="活动区域"
+          >
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">搜索</el-button>
+          <el-button>清除</el-button>
+          <el-button type="primary" icon="el-icon-plus">新增学科</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card class="box-card">
+      <div v-for="o in 4" :key="o" class="text item">
+        {{ "列表内容 " + o }}
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -7,7 +41,9 @@ export default {
   props: {},
   //数据
   data() {
-    return {};
+    return {
+      formInline: ""
+    };
   },
   //方法
   methods: {},
@@ -26,4 +62,17 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.box-card {
+  margin-bottom: 20px;
+}
+.duan {
+  width: 100px !important;
+  height: 39px;
+}
+
+.chang {
+  width: 149px !important;
+  height: 39px;
+}
+</style>
